21b8ac862826039272bede8427f21fe04a599353
Acablade <ogzylmzlr26@yahoo.com>
2021-05-06 00:32:36 +0300

    Fixed bugs

--- src/main/java/customenchants/customenchants/enchants/Firsthit.java
+++ src/main/java/customenchants/customenchants/enchants/Firsthit.java
@@ -4,9 +4,18 @@ import customenchants.customenchants.Main;
 import org.bukkit.NamespacedKey;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.enchantments.EnchantmentTarget;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.inventory.ItemStack;
 
-public class Firsthit extends Enchantment {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class Firsthit extends Enchantment implements Listener  {
     public Firsthit(String namespace){
         super(new NamespacedKey(Main.getPlugin(Main.class), namespace));
     }
@@ -50,4 +59,24 @@ public class Firsthit extends Enchantment {
     public boolean canEnchantItem(ItemStack item) {
         return true;
     }
+
+    private List<Entity> entityList = new ArrayList<>();
+
+    @EventHandler
+    public void firsthit(EntityDamageByEntityEvent event){
+        if(!(event.getDamager() instanceof Player)) return;
+        Player player = (Player) event.getDamager();
+        ItemStack itemStack = player.getItemInHand();
+        Entity entity = event.getEntity();
+        if(itemStack.getEnchantments().containsKey(Enchantment.getByKey(getKey()))){
+            if(!entityList.contains(entity)){
+                event.setDamage(event.getDamage()*2);
+                entityList.add(entity);
+            }
+            if(event.getEntity().isDead()){
+                entityList.remove(entity);
+            }
+        }
+    }
+
 }
--- src/main/java/customenchants/customenchants/enchants/Humore.java
+++ src/main/java/customenchants/customenchants/enchants/Humore.java
@@ -4,9 +4,17 @@ import customenchants.customenchants.Main;
 import org.bukkit.NamespacedKey;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.enchantments.EnchantmentTarget;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityRegainHealthEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
-public class Humore extends Enchantment {
+public class Humore extends Enchantment implements Listener {
 
     public Humore(String namespace){
         super(new NamespacedKey(Main.getPlugin(Main.class), namespace));
@@ -51,4 +59,34 @@ public class Humore extends Enchantment {
     public boolean canEnchantItem(ItemStack item) {
         return true;
     }
+
+    @EventHandler
+    public void onDamage(EntityDamageByEntityEvent event){
+        Entity entity = event.getEntity();
+
+        if(entity instanceof Player) return;
+        Player player = (Player) entity;
+
+        for(ItemStack armor : player.getInventory().getArmorContents()){
+            if(armor.getEnchantments().containsKey(Enchantment.getByKey(getKey()))){
+                if(player.getHealth()<10){
+                    player.addPotionEffect(new PotionEffect(PotionEffectType.INCREASE_DAMAGE,999999,2));
+                }
+            }
+        }
+    }
+
+    @EventHandler
+    public void humore(EntityRegainHealthEvent event){
+        if(!(event.getEntity() instanceof Player)) return;
+        Player player = (Player) event.getEntity();
+        for(ItemStack armor : player.getInventory().getArmorContents()){
+            if(armor.getEnchantments().containsKey(Enchantment.getByKey(getKey()))){
+                if(player.getHealth()>=10){
+                    player.removePotionEffect(PotionEffectType.INCREASE_DAMAGE);
+                }
+            }
+        }
+    }
+
 }
